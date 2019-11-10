a7905cd53725febfb75fda1bfc0052018dd95e08
Udith <udithshalinda2@gmail.com>
2019-11-11 03:04:20 +0530

    first commit

---
+++ src/test/test.js
@@ -0,0 +1,18 @@
+import React,{useEffect} from 'react';
+
+
+
+function Test() {
+
+    useEffect(()=>{
+
+    })
+
+  return (
+    <div >
+      <h1>hello there</h1>
+    </div>
+  );
+}
+
+export default Test;
