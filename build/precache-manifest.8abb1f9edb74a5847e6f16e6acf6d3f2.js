8b3c1312162a788f93bfc1d486ec74acaa5bc054
Udith <udithshalinda2@gmail.com>
2019-12-02 11:41:55 +0530

    build added

---
+++ build/precache-manifest.8abb1f9edb74a5847e6f16e6acf6d3f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53bf62e11a2bbfdf22c463addfc83887",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39d237c56b2f46b902c7",
+    "url": "/static/css/main.1e3a889a.chunk.css"
+  },
+  {
+    "revision": "b2e8d2878a8c9db2b083",
+    "url": "/static/js/2.d2681a30.chunk.js"
+  },
+  {
+    "revision": "39d237c56b2f46b902c7",
+    "url": "/static/js/main.fde50ee1.chunk.js"
+  },
+  {
+    "revision": "fc8c45a40b5a4d378ea2",
+    "url": "/static/js/runtime-main.98a8fde3.js"
+  }
+]);
